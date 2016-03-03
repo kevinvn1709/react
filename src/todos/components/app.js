447bac44ff8cf88d22962b5d0f6541f9640af7f0
kevinvn1709 <kevinvn.1709@gmail.com>
2016-03-03 19:06:21 +0700

    commit sample props

--- src/todos/components/app.js
+++ src/todos/components/app.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import _ from 'lodash';
 
-export default class App extends React.Component{
-    render(){
-        return(
-            <h1>Hello world from React @@@!!!!</h1>
+
+export default class App extends React.Component {
+    static defaultProps = {
+        task: [
+            {
+                id: 1,
+                task: "Make a test",
+                isCompleted: true,
+            },
+            {
+                id: 2,
+                task: "Plan a schedule",
+                isCompleted: true,
+            }
+        ]
+    }
+
+    static propTypes = {}
+
+    constructor() {
+        super();
+    }
+
+    renderTask() {
+
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Task</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    _.map(this.props.task, item => {
+                        return (
+                            <tr key={_.uniqueId()}>
+                                <td key={_.uniqueId()}>{item.task}</td>
+                                <td key={_.uniqueId()}>{item.isCompleted}</td>
+                            </tr>
+                        )
+                    })
+                }
+                </tbody>
+            </table>
         )
+    }
+
+    componentDidMount() {
+        console.log("DONE");
     }
 }
--- src/todos/index.js
+++ src/todos/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
 
-render(<App />, document.getElementById('app'));
+render(<App name="abc" value="123" autoPlay="true" />, document.getElementById('app'));
